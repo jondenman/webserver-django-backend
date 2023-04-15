93d7e72a0e4d389f82135edbd36f5de2414d06df
Jon Denman <denmanjonathan@gmail.com>
2023-04-15 08:18:22 -0700

    Added serializers.py and views.py with method to get products in JSON format. Added URL for /api/cards/

--- CardSite/views.py
+++ CardSite/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from CardSite.models import Product
+from CardSite.serializers import ProductSerializer
+from django.http import JsonResponse
 
 # Create your views here.
+def cards(request):
+    #invoke serializer
+    data = Product.objects.all()
+    serializer = ProductSerializer(data, many=True)
+    return JsonResponse({'cards': serializer.data})
--- webserver_django_backend/urls.py
+++ webserver_django_backend/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from CardSite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/cards/', views.cards, name='cards')
 ]
